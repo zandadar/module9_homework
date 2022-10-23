7f147b0d1f74132378b9781f6832850c4ce831c8
Zandadar <dartynova@yandex.ru>
2022-10-23 23:30:35 +0300

    added files

---
+++ 9.1.js
@@ -0,0 +1,39 @@
+const xmlString = `<list>
+  <student>
+    <name lang="en">
+      <first>Ivan</first>
+      <second>Ivanov</second>
+    </name>
+    <age>35</age>
+    <prof>teacher</prof>
+  </student>
+  <student>
+    <name lang="ru">
+      <first>Петр</first>
+      <second>Петров</second>
+    </name>
+    <age>58</age>
+    <prof>driver</prof>
+  </student>
+</list>`;
+
+const parser = new DOMParser();
+let result = [];
+
+const xmlDOM = parser.parseFromString(xmlString, "text/xml");
+const students = xmlDOM.querySelectorAll("student");
+
+students.forEach(node => {
+  let student = {
+    name: `$
+    {node.querySelector("first").textContent} $
+    {node.querySelector("second").textContent}`,
+    age: node.querySelector("age").textContent,
+    prof: node.querySelector("prof").textContent,
+    lang: node.querySelector("name").getAttribute("lang")
+  }
+  result.push(student);
+});
+
+console.log(result);
+
---
+++ 9.2.js
@@ -0,0 +1,18 @@
+const jsonString =`
+{
+ "list": [
+  {
+   "name": "Petr",
+   "age": "20",
+   "prof": "mechanic"
+  },
+  {
+   "name": "Vova",
+   "age": "60",
+   "prof": "pilot"
+  }
+ ]
+}`;
+
+const data = JSON.parse(jsonString);
+console.log(data);
